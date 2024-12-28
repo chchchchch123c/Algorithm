745ae08058cfe2b9065259e9a0c3498f7586fd15
chchchchch123c <chchchchch123c@gmail.com>
2024-12-28 15:08:53 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,7 @@
+sound = list(map(int, input().split()))
+if sound == sorted(sound):
+    print('ascending')
+elif sound == sorted(sound, reverse=True):
+    print('descending')
+else:
+    print('mixed')
