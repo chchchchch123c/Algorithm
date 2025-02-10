ec70d69695f2b57f0def91f4526d4f14b88cef26
chchchchch123c <chchchchch123c@gmail.com>
2025-02-10 16:30:44 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,17 @@
+n = int(input())
+for i in range(n):
+    value = input()
+    if value == 'Algorithm':
+        print('204')
+    elif value == 'DataAnalysis':
+        print('207')
+    elif value == 'ArtificialIntelligence':
+        print('302')
+    elif value == 'CyberSecurity':
+        print('B101')
+    elif value == 'Network':
+        print('303')
+    elif value == 'Startup':
+        print('501')
+    else:
+        print('105')
