31083c6382577c7b0d604a2aeac0acd84e1c18a2
chchchchch123c <chchchchch123c@gmail.com>
2025-02-14 11:41:31 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline())
+result = 0
+while n >= 0:
+    if n % 5 == 0:
+        result += n // 5
+        print(result)
+        break
+    n -= 3
+    result += 1
+else:
+    print(-1)
