415fe285cbd28a94b61a4f1ceec8cb6ebcb3921f
chchchchch123c <chchchchch123c@gmail.com>
2025-02-18 21:14:18 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,17 @@
+n = int(input())
+for i in range(n):
+    a = input()
+    stack = []
+    for i in a:
+        if i == "(":
+            stack.append(i)
+        elif i == ")":
+            if len(stack) != 0 and stack[-1] == "(":
+                stack.pop()
+            else:
+                stack.append(")")
+                break
+    if len(stack) == 0:
+        print("YES")
+    else:
+        print("NO")
