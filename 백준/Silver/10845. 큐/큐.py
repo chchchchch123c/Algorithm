703177c4babe95de4801d64d87b62a96a9c81d16
chchchchch123c <chchchchch123c@gmail.com>
2025-02-22 13:36:10 +0900

    [Silver IV] Title: 큐, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,58 @@
+'''
+좌표 정렬하기
+시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+1 초	256 MB	162878	79317	61752	48.604%
+문제
+2차원 평면 위의 점 N개가 주어진다.
+좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다.
+(-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
+
+출력
+첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
+
+예제 입력 1 
+5
+3 4
+1 1
+1 -1
+2 2
+3 3
+예제 출력 1 
+1 -1
+1 1
+2 2
+3 3
+3 4
+'''
+from sys import stdin
+stack = []
+n = int(stdin.readline())
+for i in range(n):
+    command = stdin.readline().split()
+    if command[0] == 'push':
+        stack.append(command[1])
+    elif command[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop(0))
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[0])
+    elif command[0] == 'back':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
