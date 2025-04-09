42e0e073916a9451cccefa09113af0a5021dce4c
chchchchch123c <chchchchch123c@gmail.com>
2025-04-10 08:18:34 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,12 @@
+# 입력 받기
+slogan = input()
+
+# 구호에 따른 응원 출력
+if slogan == "SONGDO":
+    print("HIGHSCHOOL")
+elif slogan == "CODE":
+    print("MASTER")
+elif slogan == "2023":
+    print("0611")
+elif slogan == "ALGORITHM":
+    print("CONTEST")
